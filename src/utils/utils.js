dc9b0b605c85decd68f90d05f987bf5218788f28
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-11-15 23:53:22 +0100

    Avancement

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,6 +10,7 @@ export const getOptimizedPath = (choices, value) => {
         "upset": 16,
     }
 
+    const initialValue = value
 
     const thirdLast = choices[0]
     const secondLast = choices[1]
@@ -21,7 +22,7 @@ export const getOptimizedPath = (choices, value) => {
     if (thirdLast) value = value - values[thirdLast]
     if (secondLast) value = value - values[secondLast]
     if (last) value = value - values[last]
-    
+
 
     while (value > 0) {
         if (value - values["upset"] >= 0) {
@@ -58,16 +59,15 @@ export const getOptimizedPath = (choices, value) => {
     while (value > 0) {
         if (value + values["shrink"] >= 0) {
             path.push("shrink")
-            value = value - values["shrink"]
+            value = value + values["shrink"]
             iterations++
-            if (path.length % 1000000 == 0) console.log(path.length)
         } else break
     }
 
     while (value > 0) {
         if (value + values["hard-hit"] >= 0) {
             path.push("hard-hit")
-            value = value - values["hard-hit"]
+            value = value + values["hard-hit"]
             iterations++
         }
         else break
@@ -76,7 +76,7 @@ export const getOptimizedPath = (choices, value) => {
     while (value > 0) {
         if (value + values["medium-hit"] >= 0) {
             path.push("medium-hit")
-            value = value - values["medium-hit"]
+            value = value + values["medium-hit"]
             iterations++
         }
         else break
@@ -85,13 +85,22 @@ export const getOptimizedPath = (choices, value) => {
     while (value > 0) {
         if (value + values["small-hit"] >= 0) {
             path.push("small-hit")
-            value = value - values["small-hit"]
+            value = value + values["small-hit"]
             iterations++
         }
         else break
     }
 
+    if (value === 1 && initialValue >= 3) {
 
+        if (value + values["small-hit"] >= 0 && value + values["punch"] >= 0) {
+            path.push("small-hit")
+            value = value + values["small-hit"]
+            iterations++
+            path.push("punch")
+            value = value + values["punch"]
+        }
+    }
 
     // console.log(value)
     // console.log(iterations)
@@ -101,7 +110,14 @@ export const getOptimizedPath = (choices, value) => {
     secondLast && path.push(secondLast)
     last && path.push(last)
 
-    console.log(path)
+    if (value != 0) {
+        console.log("Erreur: " + value)
+        return
+    }
+    // console.log("Nombre d'itérations: " + iterations)
+    console.log("Valeur demandée: " + initialValue)
+    console.log("Chemin: " + path)
+
     return path
 }
 
