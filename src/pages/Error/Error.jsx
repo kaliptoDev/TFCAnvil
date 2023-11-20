7920f6e51c6e02454109dfbe58dfa6735a7c6e55
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-11-20 08:16:06 +0100

    Error page removed for a navigate back to mainpage

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,14 +1,18 @@
 import './Error.css'
+import { Navigate } from 'react-router-dom'
 
 const Error = () => {
 
     return (
-        <div className="Error">
-            <h1>This is an error page.</h1>
-            <div className='error__link'>
-                <a href="/">Go back to the home page</a>
-            </div>
-        </div>
+
+        <Navigate to="/" />
+
+        // <div className="Error">
+        //     <h1>This is an error page.</h1>
+        //     <div className='error__link'>
+        //         <a href="/">Go back to the home page</a>
+        //     </div>
+        // </div>
     )
 }
 
