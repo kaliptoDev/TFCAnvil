d86cfe0fe7043f1d23515188c757aa445a042feb
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-11-20 08:58:58 +0100

    Added Footer Added Footer and Header into RoutesTree Now they appear everywhere as expected

--- src/router/RoutesTree.jsx
+++ src/router/RoutesTree.jsx
@@ -1,14 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Error from '../pages/Error/Error';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 const RoutesTree = () => {
 
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<Error />} />
-        </Routes>
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+            <Footer />
+        </>
     )
 }
 
