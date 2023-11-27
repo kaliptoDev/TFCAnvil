6f5e4a5d158bfadfc0543dbf2a1f3da4bb7ba981
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-11-27 08:02:41 +0100

    Added support for scrolled menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,57 @@
 import "./Header.css"
-
-import { Link } from "react-router-dom"
+import { useState } from "react"
 
 const Header = () => {
 
+    const [TFCToolsStyle, setTFCToolsStyle] = useState('')
+
+    const [TFCToolsChevronStyle, setTFCToolsChevronStyle] = useState('')
+    // const [MCToolsStyle, setMCToolsStyle] = useState('')
+    const [MCToolsChevronStyle, setMCToolsChevronStyle] = useState('')
+    // const [ResourcesStyle, setResourcesStyle] = useState('')
+    const [ResourcesChevronStyle, setResourcesChevronStyle] = useState('')
+
+
+
     return (
         <div className="header">
-            <div className="header__background"></div>
-            <div className="header__logo"></div>
+            {/* <div className="header__background"></div>
+            <div className="header__logo"></div> */}
 
+            <div className="header__menu__title">
+                <h1 className="header__main__title">MC Tools</h1>
+            </div>
 
             <div className="header__menu">
                 <div className="header__menu__item">
                     <a href="/" className="header__link">
                         <h1 className="header__title">TFC Tools</h1>
                     </a>
-                    <div className="header__menu__item__arrow">
-                        <i className="header__chevron"></i>
+                    <div className="header__menu__item__arrow"
+                        onClick={() => {
+                            if (TFCToolsStyle === '') {
+                                setTFCToolsStyle('header__sub__menu__active')
+                                setTFCToolsChevronStyle('header__chevron__active')
+                            } else {
+                                setTFCToolsStyle('')
+                                setTFCToolsChevronStyle('')
+                            }
+                        }}
+                        onKeyDown={(event) => {
+                            if (event.key === 'Enter' || event.key === ' ') {
+                                if (TFCToolsStyle === '') {
+                                    setTFCToolsStyle('header__sub__menu__active')
+                                    setTFCToolsChevronStyle('header__chevron__active')
+                                } else {
+                                    setTFCToolsStyle('')
+                                    setTFCToolsChevronStyle('')
+                                }
+                            }
+                        }}
+                    >
+                        <img src="/svgs/chevron.svg" alt="chevron" className={`header__chevron ${TFCToolsChevronStyle}`} />
                     </div>
-                    <div className="header__menu__item__sub">
+                    <div className={`header__menu__item__sub ${TFCToolsStyle}`}>
                         <div className="header__menu__item__sub__item">
                             <a href="/" className="header__link">
                                 <h1 className="header__title">Anvil Calculator</h1>
@@ -35,8 +68,26 @@ const Header = () => {
                     <a href="/" className="header__link">
                         <h1 className="header__title">Minecraft Tools</h1>
                     </a>
-                    <div className="header__menu__item__arrow">
-                        <i className="header__chevron"></i>
+                    <div className="header__menu__item__arrow"
+                        onClick={() => {
+                            if (MCToolsChevronStyle === '') {
+                                setMCToolsChevronStyle('header__chevron__active')
+                            } else {
+                                setMCToolsChevronStyle('')
+                            }
+                        }
+                        }
+                        onKeyDown={(event) => {
+                            if (event.key === 'Enter' || event.key === ' ') {
+                                if (MCToolsChevronStyle === '') {
+                                    setMCToolsChevronStyle('header__chevron__active')
+                                } else {
+                                    setMCToolsChevronStyle('')
+                                }
+                            }
+                        }}
+                    >
+                        <img src="/svgs/chevron.svg" alt="chevron" className={`header__chevron ${MCToolsChevronStyle}`} />
                     </div>
                 </div>
 
@@ -44,8 +95,26 @@ const Header = () => {
                     <a href="/" className="header__link">
                         <h1 className="header__title">Resources</h1>
                     </a>
-                    <div className="header__menu__item__arrow">
-                        <i className="header__chevron"></i>
+                    <div className="header__menu__item__arrow"
+                        onClick={() => {
+                            if (ResourcesChevronStyle === '') {
+                                setResourcesChevronStyle('header__chevron__active')
+                            } else {
+                                setResourcesChevronStyle('')
+                            }
+                        }
+                        }
+                        onKeyDown={(event) => {
+                            if (event.key === 'Enter' || event.key === ' ') {
+                                if (ResourcesChevronStyle === '') {
+                                    setResourcesChevronStyle('header__chevron__active')
+                                } else {
+                                    setResourcesChevronStyle('')
+                                }
+                            }
+                        }}
+                    >
+                        <img src="/svgs/chevron.svg" alt="chevron" className={`header__chevron ${ResourcesChevronStyle}`} />
                     </div>
                 </div>
 
@@ -61,7 +130,21 @@ const Header = () => {
                     </a>
                 </div>
 
+                <div className="header__connection">
+                    <div className="header__connection__item header__menu__item">
+                        <a href="/" className="header__link">
+                            <h1 className="header__title">Login</h1>
+                        </a>
+                    </div>
+                    <div className="header__connection__item header__menu__item">
+                        <a href="/" className="header__link">
+                            <h1 className="header__title">Register</h1>
+                        </a>
+                    </div>
+                </div>
+
             </div>
+
         </div>
 
     )
