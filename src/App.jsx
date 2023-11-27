28d5783d324de52b8a339bd78caaad340a76f5cb
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-11-27 08:03:00 +0100

    Removed useless imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
 import './App.css'
 import RoutesTree from './router/RoutesTree'
 import { BrowserRouter as Router } from 'react-router-dom'
-import UserRequierementsChoicesProvider from './providers/UserRequierementsChoices'
-import CursorPositionProvider from './providers/CursorPositionProvider'
 
 function App() {
 
